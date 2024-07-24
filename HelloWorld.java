ea3a0807e0c57f38592280b08e47c1a3cea92f81
Gerson Solimon <solimon.gerson@hotmail.com>
2024-07-24 15:23:00 +0200

    yes i can

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[]args){
-        System.out.println("hello Man");
+        System.out.println("hello this is me");
+        
     }
 }
