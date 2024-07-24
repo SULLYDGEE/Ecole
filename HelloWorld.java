f1eaeb6d8a9ad781153017d09001f668edf4bad5
Gerson Solimon <solimon.gerson@hotmail.com>
2024-07-24 15:28:19 +0200

    hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[]args){
-        System.out.println("hello this is me");
+        System.out.println("hello how are you man");
         
     }
 }
